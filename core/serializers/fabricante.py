2a1562dce3298306bf37303f0f5e74ce13ac0c37
matheushodecker <matheus.hodecker.santos@gmail.com>
2025-06-16 19:53:29 -0300

    feat: remoção da model fabricante e da api para fabricante

--- core/serializers/fabricante.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from core.models import Fabricante
-
-
-class FabricanteSerializer(ModelSerializer):
-    class Meta: 
-        model = Fabricante
-        fields = '__all__'
